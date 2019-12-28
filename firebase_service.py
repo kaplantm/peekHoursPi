b91c7a9c31ad20ceb94e5f89174a8169e6deabe9
Toni Kaplan <tonarie.k@gmail.com>
2019-12-28 02:16:39 -0500

    increment working

--- firebase_service.py
+++ firebase_service.py
@@ -1,6 +1,8 @@
 import firebase_admin
+from google.cloud.firestore_v1 import Increment
 from firebase_admin import credentials
 from firebase_admin import firestore
+
 
 # years - total times triggered that year
 # months - total times triggered that month
@@ -12,81 +14,73 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-
-def delete_collection(coll_ref, batch_size):
-    docs = coll_ref.limit(batch_size).get()
-    deleted = 0
-
-    for doc in docs:
-        print(u'Deleting doc {} => {}'.format(doc.id, doc.to_dict()))
-        doc.reference.delete()
-        deleted = deleted + 1
-
-    if deleted >= batch_size:
-        return delete_collection(coll_ref, batch_size)
-
-
 def add_room(room_name):
     print("add_room")
     doc_ref = db.collection(u'rooms').document(room_name)
     doc_ref.set({})
 
-
-def add_or_update_room_aggregate_without_recalculation(room_name, aggregate_type, aggregate_label, value):
-    print ("update_room")
-    doc_ref = db.collection(u'rooms').document(room_name).collection(aggregate_type).document(aggregate_label)
-    doc_ref.set({u'value': value})
-
-
-def add_or_update_room_year_aggregate(room_name, year, usage):
-    print ("update_room_year")
-    doc_ref = db.collection(u'rooms').document(room_name).collection('years').document(str(year))
-    doc_ref.set({
-        u'year': year,
-        u'usage': usage
-    })
-
-
-def add_or_update_room_month_aggregate(room_name, year, month, usage):
-    print ("update_room_month")
-    doc_ref = db.collection(u'rooms').document(room_name).collection('years').document(str(year)).collection('months')\
-        .document(str(month))
-    doc_ref.set({
-        u'month': month,
-        u'usage': usage
-    })
-
-def add_or_update_room_day_aggregate(room_name, year, month, day, usage):
+@firestore.transactional
+def increment_usage(transaction, room_name, year, month, day, hour, increment_amount):
     # TODO make sure parent collection exists
     print ("update_room_day")
-    doc_ref = db.collection(u'rooms').document(room_name).collection('years').document(str(year)).collection('months')\
-        .document(str(month)).collection('days').document(str(day))
-    doc_ref.set({
-        u'day': day,
-        u'usage': usage
+    room_ref = db.collection(u'rooms').document(room_name)
+    year_ref = room_ref.collection(u'years').document(str(year))
+    month_ref = year_ref.collection(u'months').document(str(month))
+    day_ref = month_ref.collection(u'days').document(str(day))
+    hour_ref = day_ref.collection(u'hours').document(str(hour))
+
+    year_snapshot = year_ref.get(transaction=transaction)
+    month_snapshot = month_ref.get(transaction=transaction)
+    day_snapshot = day_ref.get(transaction=transaction)
+    hour_snapshot = hour_ref.get(transaction=transaction)
+
+    old_year_usage = year_snapshot.get(u'usage');
+    old_month_usage = month_snapshot.get(u'usage');
+    old_day_usage = day_snapshot.get(u'usage');
+    old_hour_usage = hour_snapshot.get(u'usage');
+
+    print("old_hour_usage", old_hour_usage)
+    transaction.set(year_ref, {
+        u'usage': old_year_usage + 1 if old_year_usage else 1,
+    }),
+    transaction.set(month_ref, {
+        u'usage': old_month_usage + 1 if old_month_usage else 1,
+    })
+    transaction.set(day_ref, {
+        u'usage': old_day_usage + 1 if old_day_usage else 1,
+    })
+    transaction.set(hour_ref, {
+        u'usage': old_hour_usage + 1 if old_hour_usage else 1,
     })
 
 
-def delete_room(room_name):
-    print ("delete")
-    years_ref = db.collection(u'rooms').document(room_name).collection('years')
-    months_ref = db.collection(u'rooms').document(room_name).collection('months')
-    days_ref = db.collection(u'rooms').document(room_name).collection('days')
-    room_ref = db.collection(u'rooms').document(room_name)
-
-    delete_collection(years_ref, 50)
-    delete_collection(months_ref, 50)
-    delete_collection(days_ref, 50)
-    room_ref.delete()
-
-
-def get_room_aggregate_data(room_name, aggregate_type):
-    print ("get_room_aggregate_data", room_name, aggregate_type)
-    data_ref = db.collection(u'rooms').document(room_name).collection(aggregate_type)
+def get_room_aggregate_year_data(room_name):
+    data_ref = db.collection(u'rooms').document(room_name).collection(u'years')
     docs = data_ref.stream()
+    printDocs(docs)
+
+
+def get_room_aggregate_month_data(room_name, year):
+    data_ref = db.collection(u'rooms').document(room_name).collection(u'years').document(str(year)).collection(u'months')
+    docs = data_ref.stream()
+
+    printDocs(docs)
+
+def get_room_aggregate_day_data(room_name, year, month):
+    data_ref = db.collection(u'rooms').document(room_name).collection(u'years').document(str(year)).collection(u'months')\
+        .document(str(month)).collection(u'days')
+    docs = data_ref.stream()
+    printDocs(docs)
+
+def get_room_aggregate_hour_data(room_name, year, month, day):
+    data_ref = db.collection(u'rooms').document(room_name).collection(u'years').document(str(year)).collection(u'months')\
+        .document(str(month)).collection(u'days').document(str(day)).collection(u'hours')
+    docs = data_ref.stream()
+    printDocs(docs)
+
+def printDocs(docs):
     for doc in docs:
         print(u'{} => {}'.format(doc.id, doc.to_dict()))
-
 
 def get_all_rooms():
     print ("get_all_rooms")
--- gui.py
+++ gui.py
@@ -8,15 +8,30 @@ def init(room):
 
     tk.Button(root, text="QUIT", fg="red", command=quit).grid(row=1, column=0)
     tk.Button(root, text="add_room", command=lambda: firebase_service.add_room(room)).grid(row=2, column=0)
-    tk.Button(root, text="delete_room", command=lambda: firebase_service.delete_room(room)).grid(row=3, column=0)
 
-    # tk.Button(root, text="add_or_update_room_aggregate year", command=lambda: firebase_service.add_or_update_room_aggregate(room, 'year', '1995', .5)).grid(row=1, column=0)
-    tk.Button(root, text="add_or_update_room_year_aggregate", command=lambda: firebase_service.add_or_update_room_year_aggregate(room, 2019, .5)).grid(row=1, column=1)
-    tk.Button(root, text="add_or_update_room_month_aggregate", command=lambda: firebase_service.add_or_update_room_month_aggregate(room, 2019, 10, .25)).grid(row=2, column=1)
-    tk.Button(root, text="add_or_update_room_day_aggregate", command=lambda: firebase_service.add_or_update_room_day_aggregate(room, 2019, 10, 1, .5)).grid(row=3, column=1)
+    tk.Button(root, text="increment_usage 10/1/2019 3PM",
+              command=lambda: firebase_service.increment_usage(firebase_service.db.transaction(),
+                                                               room, 2019, 10, 1, 1, 15)).grid(row=1, column=1)
+    tk.Button(root, text="increment_usage 10/2/2019 4PM",
+              command=lambda: firebase_service.increment_usage(firebase_service.db.transaction(),
+                                                               room, 2019, 10, 2, 1, 16)).grid(row=2, column=1)
+    tk.Button(root, text="increment_usage 11/2/2019 5PM",
+              command=lambda: firebase_service.increment_usage(firebase_service.db.transaction(),
+                                                               room, 2019, 11, 2, 1, 17)).grid(row=3, column=1)
+    tk.Button(root, text="increment_usage 10/1/2020 6AM",
+              command=lambda: firebase_service.increment_usage(firebase_service.db.transaction(),
+                                                               room, 2020, 10, 1, 1, 6)).grid(row=4, column=1)
 
-    # tk.Button(root, text="get_room_year_data", command=lambda: firebase_service.get_room_aggregate_data(room, 'year')).grid(row=1, column=0).grid(row=2, column=2)
     tk.Button(root, text="get_all_rooms", command=firebase_service.get_all_rooms).grid(row=1, column=2)
+    tk.Button(root, text="get_room_years_data", command=lambda: firebase_service.get_room_aggregate_year_data(room))\
+        .grid(row=2, column=2)
+    tk.Button(root, text="get_room_months_data 2019", command=lambda: firebase_service.
+              get_room_aggregate_month_data(room, 2019)).grid(row=3, column=2)
+    tk.Button(root, text="get_room_days_data 2019 10", command=lambda: firebase_service.
+              get_room_aggregate_day_data(room, 2019, 10)).grid(row=4, column=2)
+    tk.Button(root, text="get_room_hours_data 2019 10 1", command=lambda: firebase_service.
+              get_room_aggregate_hour_data(room, 2019, 10, 1)).grid(row=5, column=2)
+
 
     root.mainloop()
 
